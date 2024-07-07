d19aaef036cb741b75c0fe3a5d65477886c54193
Minerdev1103 <121298556+Minerdev1103@users.noreply.github.com>
2024-07-07 20:43:29 +0700

    Update RESUME

---
+++ script.js
@@ -0,0 +1,4 @@
+//<video poster="https://videos.pexels.com/video-files/2611250/2611250-uhd_2560_1440_30fps.jpg" playsinline autoplay muted loop >
+  //      <source src="https://videos.pexels.com/video-files/2611250/2611250-uhd_2560_1440_30fps.webm" type="video/webm">
+    //    <source src="https://videos.pexels.com/video-files/2611250/2611250-uhd_2560_1440_30fps.mp4" type="video/mp4">
+    //</video></source>
