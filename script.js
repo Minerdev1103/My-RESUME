21fd48318fde739578a143c443561e920c3533e3
Minerdev1103 <121298556+Minerdev1103@users.noreply.github.com>
2024-07-08 14:12:59 +0700

    update

--- script.js
+++
@@ -1,4 +0,0 @@
-//<video poster="https://videos.pexels.com/video-files/2611250/2611250-uhd_2560_1440_30fps.jpg" playsinline autoplay muted loop >
-  //      <source src="https://videos.pexels.com/video-files/2611250/2611250-uhd_2560_1440_30fps.webm" type="video/webm">
-    //    <source src="https://videos.pexels.com/video-files/2611250/2611250-uhd_2560_1440_30fps.mp4" type="video/mp4">
-    //</video></source>
